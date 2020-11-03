271e1b36f66a75afa45e544528a5748423c500a1
4L3J0 <alejosegi@gmail.com>
2020-11-02 22:10:11 -0500

    Se crearón modelos y migraciones

---
+++ database/migrations/2020_11_03_023911_create_product_carts_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductCartsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product_carts', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('product_id')
+                    ->constrained()
+                    ->onDelete('cascade');
+            $table->foreignId('cart_id')
+                    ->constrained()
+                    ->onDelete('cascade');
+            $table->integer('quantity');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product_carts');
+    }
+}
